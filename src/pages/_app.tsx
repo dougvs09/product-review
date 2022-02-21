b43488f887acebbefa0860c0f2f2259fc291976d
Douglas Valente <douglasvalente6@gmail.com>
2022-02-21 18:32:40 -0300

    feat: add global styles for the pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app';
 
 import { AuthContextProvider } from '@contexts/AuthContext';
+import { globalStyles } from '@styles/GlobalStyles';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <AuthContextProvider>
-    <Component {...pageProps} />
-  </AuthContextProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  globalStyles();
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
+};
 
 export default MyApp;
