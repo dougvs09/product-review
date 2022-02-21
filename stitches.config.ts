8d600393c3739c50e898d2774a92fe53013b4ae8
Douglas Valente <douglasvalente6@gmail.com>
2022-02-21 18:28:57 -0300

    chore: add stitches config

---
+++ stitches.config.ts
@@ -0,0 +1,61 @@
+import { createStitches } from '@stitches/react';
+
+export const {
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  styled,
+  theme,
+} = createStitches({
+  theme: {
+    colors: {
+      purple100: '#9D4EDD',
+      purple200: '#5A189A',
+      purple300: '#240046',
+
+      black100: '#222',
+      black200: '#111',
+      black300: '#000',
+
+      gray100: '#9B9B9B',
+      gray200: '#4E4E4E',
+      gray300: '#383838',
+
+      white100: '#FFFFFF',
+      white200: '#EDEDED',
+      white300: '#DBDBDB',
+
+      red100: '#FF0000',
+
+      green100: '#00A400',
+    },
+    fontSizes: {
+      1: '12px',
+      2: '16px',
+      3: '24px',
+      4: '32px',
+      5: '48px',
+    },
+    fonts: {
+      sansSerif: 'Inter, -apple-system, system-ui, sans-serif',
+    },
+    fontWeights: {
+      regular: '400',
+      medium: '500',
+      semiBold: '600',
+      bold: '700',
+    },
+  },
+  utils: {
+    mx: (value: number) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    my: (value: number) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+  },
+});
