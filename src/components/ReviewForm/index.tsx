7438a174a8d4293efc9d4b55831e95452d371f15
Douglas Valente <douglasvalente6@gmail.com>
2022-02-24 11:10:25 -0300

    feat: add input for rate and loading when review is being created

---
+++ src/components/ReviewForm/index.tsx
@@ -0,0 +1,204 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { MdClear } from 'react-icons/md';
+
+import {
+  Button,
+  Clear,
+  Errors,
+  Form,
+  InputGroup,
+  Label,
+  SelectGroup,
+  Spinner,
+  TextareaGroup,
+} from './styles';
+
+export type FormTypes = {
+  title: string;
+  description: string;
+  brand: string;
+  dayOfPurchase: string;
+  category: string;
+  price: string;
+  rate: string;
+};
+
+type ReviewFormTypes = {
+  handleCreateReview: SubmitHandler<FormTypes>;
+  loading: boolean;
+};
+
+export const ReviewForm: React.FC<ReviewFormTypes> = ({
+  handleCreateReview,
+  loading,
+}: ReviewFormTypes) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    resetField,
+  } = useForm<FormTypes>();
+
+  const category = watch('category');
+
+  const clearInput = (
+    name: 'price' | 'title' | 'description' | 'dayOfPurchase' | 'rate'
+  ) => {
+    resetField(name);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(handleCreateReview)}>
+      <InputGroup>
+        <Label required htmlFor="title">
+          Title
+        </Label>
+        <input
+          placeholder="Insira aqui o nome do produto"
+          {...register('title', { required: true })}
+        />
+        {errors.title && <Errors>Is required</Errors>}
+        <Clear type="button" onClick={() => clearInput('title')}>
+          <MdClear color="#FFF" />
+        </Clear>
+      </InputGroup>
+
+      <TextareaGroup>
+        <Label required htmlFor="description">
+          Description
+        </Label>
+        <textarea
+          placeholder="Insira aqui uma descrição breve sobre o produto e o que você achou dele"
+          {...register('description', { required: true })}
+        />
+        {errors.description && <Errors>Is required</Errors>}
+        <Clear type="button" onClick={() => clearInput('description')}>
+          <MdClear color="#FFF" />
+        </Clear>
+      </TextareaGroup>
+
+      <InputGroup>
+        <Label required htmlFor="price">
+          Price
+        </Label>
+        <input
+          type="number"
+          placeholder="R$ 1.999,99"
+          {...register('price', {
+            required: true,
+            pattern: /([0-9]{1,3}[,])([0-9]{1})/,
+          })}
+        />
+        {errors.price?.type === 'required' && <Errors>Is required</Errors>}
+        {errors.price?.type === 'pattern' && (
+          <Errors>Formato aceito: ...000,00</Errors>
+        )}
+        <Clear type="button" onClick={() => clearInput('price')}>
+          <MdClear color="#FFF" />
+        </Clear>
+      </InputGroup>
+
+      <InputGroup>
+        <Label required htmlFor="rate">
+          Rate
+        </Label>
+        <input
+          placeholder="Coloque uma nota de 1,0 a 5,0"
+          {...register('rate', {
+            required: true,
+            pattern: /([0-9]{1}[,])([0-9]{2})/,
+          })}
+        />
+        {errors.price?.type === 'required' && <Errors>Is required</Errors>}
+        {errors.price?.type === 'pattern' && (
+          <Errors>Formato aceito: 0,00</Errors>
+        )}
+        <Clear type="button" onClick={() => clearInput('rate')}>
+          <MdClear color="#FFF" />
+        </Clear>
+      </InputGroup>
+
+      <SelectGroup>
+        <Label required htmlFor="category">
+          Category
+        </Label>
+        <select
+          defaultValue="DEFAULT"
+          {...register('category', {
+            validate: (v) => v !== 'Choose your category',
+          })}
+        >
+          <option value="DEFAULT" hidden disabled>
+            Choose your category
+          </option>
+          <option value="smartphone">Smartphone</option>
+          <option value="television">Television</option>
+          <option value="notebook">Notebook</option>
+        </select>
+        {errors.category && <Errors>Is required</Errors>}
+      </SelectGroup>
+
+      <SelectGroup>
+        <Label required htmlFor="brand">
+          Brand
+        </Label>
+        <select
+          defaultValue="DEFAULT"
+          {...register('brand', {
+            validate: (v) => v !== 'Choose your brand',
+          })}
+        >
+          <option value="DEFAULT" hidden disabled>
+            Choose your brand
+          </option>
+          {category === 'smartphone' && (
+            <>
+              <option value="samsung">Samsung</option>
+              <option value="apple">Apple</option>
+              <option value="xiaomi">Xiaomi</option>
+              <option value="motorola">Motorola</option>
+              <option value="other">Other</option>
+            </>
+          )}
+          {category === 'notebook' && (
+            <>
+              <option value="dell">Dell</option>
+              <option value="apple">Apple</option>
+              <option value="vaio">Vaio</option>
+              <option value="samsung">Samsung</option>
+              <option value="lenovo">Lenovo</option>
+              <option value="other">Other</option>
+            </>
+          )}
+          {category === 'television' && (
+            <>
+              <option value="samsung">Samsung</option>
+              <option value="lg">LG</option>
+              <option value="philco">Philco</option>
+              <option value="sony">Sony</option>
+              <option value="other">Other</option>
+            </>
+          )}
+        </select>
+        {errors.brand && <Errors>Is required</Errors>}
+      </SelectGroup>
+
+      <InputGroup>
+        <Label required htmlFor="dayOfPurchase">
+          Day of purchase
+        </Label>
+        <input
+          type="date"
+          {...register('dayOfPurchase', {
+            required: true,
+          })}
+        />
+        {errors.dayOfPurchase && <Errors>Is required</Errors>}
+      </InputGroup>
+      <Button color="purple" size="all" loading={loading} type="submit">
+        {!loading ? 'Enviar' : <Spinner />}
+      </Button>
+    </Form>
+  );
+};
---
+++ src/components/ReviewForm/styles.ts
@@ -0,0 +1,243 @@
+import { shade } from 'polished';
+
+import { keyframes, styled } from '../../../stitches.config';
+
+export const Form = styled('form', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '20px',
+
+  maxWidth: '1000px',
+  padding: '0 20px',
+});
+
+export const InputGroup = styled('div', {
+  width: '400px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '5px',
+
+  position: 'relative',
+
+  input: {
+    maxWidth: '400px',
+    padding: '10px 7px',
+    borderRadius: '8px',
+    border: '2px solid $purple100',
+    outline: 'none',
+
+    transition: '0.2s',
+
+    fontFamily: '$sansSerif',
+    fontWeight: '$medium',
+    fontSize: '$2',
+
+    '&:focus': {
+      borderColor: '$purple200',
+    },
+
+    '&::placeholder': {
+      color: '$gray100',
+    },
+  },
+});
+
+export const SelectGroup = styled('div', {
+  width: '400px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '5px',
+
+  select: {
+    maxWidth: '400px',
+    padding: '10px 5px',
+    borderRadius: '8px',
+    border: '2px solid $purple100',
+    outline: 'none',
+
+    fontFamily: '$sansSerif',
+    fontWeight: '$medium',
+    fontSize: '$2',
+  },
+
+  option: {
+    maxWidth: '400px',
+    padding: '10px 5px',
+    borderRadius: '8px',
+    border: '2px solid $purple100',
+    outline: 'none',
+
+    fontFamily: '$sansSerif',
+    fontWeight: '$medium',
+    fontSize: '$2',
+  },
+});
+
+export const TextareaGroup = styled('div', {
+  width: '400px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '5px',
+
+  position: 'relative',
+
+  textarea: {
+    maxWidth: '400px',
+    height: '200px',
+    resize: 'none',
+    padding: '10px 23px 10px 5px',
+    borderRadius: '8px',
+    border: '2px solid $purple100',
+    outline: 'none',
+
+    transition: '0.2s',
+
+    fontFamily: '$sansSerif',
+    fontWeight: '$medium',
+    fontSize: '$2',
+
+    '&:focus': {
+      borderColor: '$purple200',
+    },
+
+    '&::placeholder': {
+      color: '$gray100',
+    },
+  },
+});
+
+export const Label = styled('label', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '5px',
+
+  fontFamily: '$sansSerif',
+  fontWeight: '$semiBold',
+  fontSize: '$2',
+
+  variants: {
+    required: {
+      true: {
+        '&::after': {
+          content: '*',
+          color: '$red100',
+        },
+      },
+    },
+  },
+});
+
+export const Errors = styled('span', {
+  fontWeight: '$semiBold',
+  fontSize: '$1',
+  color: '$red100',
+});
+
+export const Clear = styled('button', {
+  display: 'flex',
+  alignItems: 'center',
+
+  width: '19px',
+  height: '19px',
+  paddingLeft: '3px',
+
+  borderRadius: '50%',
+  border: 'none',
+
+  background: '$purple200',
+
+  position: 'absolute',
+  top: '36px',
+  left: '366px',
+
+  transition: '0.3s ease-in-out',
+
+  '&:hover': {
+    background: shade(0.2, '#5A189A'),
+  },
+});
+
+export const Button = styled('button', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  border: 'none',
+  outline: 'none',
+  borderRadius: '8px',
+  padding: '10px',
+
+  fontFamily: '$sansSerif',
+  fontWeight: '$medium',
+  fontSize: '$2',
+
+  variants: {
+    color: {
+      purple: {
+        background: '$purple100',
+        color: '$black300',
+
+        transition: '0.3s',
+
+        '&:hover': {
+          background: shade(0.1, '#9D4EDD'),
+        },
+      },
+      red: {
+        background: '$red100',
+        color: '$white100',
+
+        transition: '0.3s',
+
+        '&:hover': {
+          background: shade(0.1, '#ff0000'),
+        },
+      },
+      green: {
+        background: '$green100',
+        color: '$white100',
+
+        transition: '0.3s',
+
+        '&:hover': {
+          background: shade(0.1, '#00a400'),
+        },
+      },
+    },
+    size: {
+      100: {
+        maxWidth: '100px',
+      },
+      200: {
+        maxWidth: '200px',
+      },
+      300: {
+        maxWidth: '300px',
+      },
+      all: {
+        width: '100%',
+      },
+    },
+    loading: {
+      true: {
+        cursor: 'not-allowed',
+      },
+    },
+  },
+});
+
+const spinn = keyframes({
+  '0%': { transform: 'rotate(360deg)' },
+});
+
+export const Spinner = styled('div', {
+  borderRadius: '50%',
+  border: '3px solid rgba(0,0,0,0.1)',
+  borderLeftColor: '$white100',
+  width: '25px',
+  height: '25px',
+  animation: `${spinn} 0.5s linear infinite`,
+});
