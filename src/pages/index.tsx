47314da8f8d375fef720a0da6243096d3f9819e2
Douglas Valente <douglasvalente6@gmail.com>
2022-03-04 19:47:11 -0300

    feat: add header in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => <h1>Homepage</h1>;
+import { Header } from '@components/Header';
+
+import { styled } from '../../stitches.config';
+
+const Container = styled('div', {
+  position: 'relative',
+  width: '100%',
+  height: '100vh',
+});
+
+const Home: NextPage = () => (
+  <Container>
+    <Header />
+  </Container>
+);
 
 export default Home;
