6e7586885fb2f88880fc272e3fd5c6f6a76eb745
Douglas Valente <douglasvalente6@gmail.com>
2022-03-07 18:53:15 -0300

    feat: add review card components in the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
+import { useQuery } from 'react-query';
+
 import type { NextPage } from 'next';
 
 import { Header } from '@components/Header';
+import { ReviewCard } from '@components/ReviewCard';
+import { api } from 'utils/api';
 
 import { styled } from '../../stitches.config';
+
+type ReviewTypes = {
+  id: string;
+  name: string;
+  author: {
+    name: string;
+  };
+  createdAt: string;
+  pictureUrl: string;
+};
 
 const Container = styled('div', {
   position: 'relative',
@@ -10,10 +24,34 @@ const Container = styled('div', {
   height: '100vh',
 });
 
-const Home: NextPage = () => (
-  <Container>
-    <Header />
-  </Container>
-);
+const ReviewsContainer = styled('section', {
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  maxWidth: '1200px',
+  margin: '50px auto 0 auto',
+  padding: '0 20px',
+});
+
+const Home: NextPage = () => {
+  const { data: review } = useQuery<ReviewTypes[], Error>(
+    'reviews',
+    async () => {
+      const response = await api.get('/review');
+      return response.data;
+    }
+  );
+
+  return (
+    <Container>
+      <Header />
+      <ReviewsContainer>
+        {review?.map((data) => (
+          <ReviewCard key={data.id} data={data} />
+        ))}
+      </ReviewsContainer>
+    </Container>
+  );
+};
 
 export default Home;
